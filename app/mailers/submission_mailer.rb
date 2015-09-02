ccc43934726e23310c35563720ee9d6ff57c56a1
Jonathan (Jonny) Johannes <jonny@jonnyjohannes.com>
2015-09-02 08:25:53 -0600

    add bcc to submission mailer, change the donotreply email.

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -1,9 +1,13 @@
 class SubmissionMailer < ApplicationMailer
-  default from: 'donotreply@iqst.ca'
+  default from: 'donotreply@phasdga.ca'
 
   def confirmation_email(submission)
     @submission = submission
 
-    mail(to: @submission.email, subject: 'PHAS Symposium 2015 Registration Confirmation')
+    mail(
+      to: @submission.email,
+      subject: 'PHAS Symposium 2015 Registration Confirmation',
+      bcc: ['jonny.p.johannes@ucalgary.ca'],
+    )
   end
 end

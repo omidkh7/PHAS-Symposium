7a470045b6a5ac92ce9a20a46f7edf11677fc9e1
Jonny Johannes <jonny@jonnyjohannes.com>
2017-02-19 15:33:25 -0700

    Accomodate for ordered poster presentation id definition.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,15 +1,17 @@
 class SubmissionsController < ApplicationController 
 
   ORAL_PRESENTATION_ORDERED_IDS = [101, 81, 80, 76, 93, 95, 106, 87, 65, 99, 91, 89, 102, 92, 105, 94].freeze
+  POSTER_PRESENTATION_ORDERED_IDS = [].freeze
 
   def index
     begin
       @oral_presentations = ORAL_PRESENTATION_ORDERED_IDS.collect { |id| Submission.find(id) }
+      @poster_presentations = POSTER_PRESENTATION_ORDERED_IDS.collect { |id| Submission.find(id) }
     rescue
       @oral_presentations = Submission.orals
+      @poster_presentations = Submission.posters
     end
 
-    @poster_presentations = Submission.posters
     render 'index'
   end
 
